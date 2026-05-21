e042d0a7b56478ed6bbeccb5aa0af5053d0f1cdd
Saksham Panwar <skshmpnwar@gmail.com>
2026-05-21 22:00:59 +0530

    Implement basic traffic light control logic

--- Traffic_Lights_3_LEDs.ino
+++ Traffic_Lights_3_LEDs.ino
@@ -1,1 +1,22 @@
+int red = 10;
+int yellow = 9;
+int green = 8;
 
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+void loop() {
+  digitalWrite(red, HIGH);
+  delay(3000);
+  digitalWrite(red, LOW);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow, LOW);
+  
+  digitalWrite(green, HIGH);
+  delay(3000);
+  digitalWrite(green, LOW);
+}
